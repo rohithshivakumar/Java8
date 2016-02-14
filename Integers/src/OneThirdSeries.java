01dc11c2bbae45388d1a749b8ec0ad3eeff2e664
rohithshivakumar <srohith85@gmail.com>
2016-02-13 20:52:56 -0800

    One third series problem statement

---
+++ Integers/src/OneThirdSeries.java
@@ -0,0 +1,19 @@
+package src;
+
+/**
+ * Given a number series, return true if there is even one number that appears more than 1/3rd the length of the series.
+ * Else false.
+ *
+ * Clarifications
+ * Integers (+ve and -ve)
+ * Unsorted
+ * Only constant additional space allowed (variables temporary)
+ *
+ *
+ * Example :
+ *
+ *
+ */
+
+public class OneThirdSeries {
+}
