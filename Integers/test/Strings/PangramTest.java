9c9179c60c243c4610194f287d52f207c5feb096
arjun007r <arjundude007@gmail.com>
2016-03-08 22:29:29 -0800

    Pangram question checkin with test class

---
+++ Integers/test/Strings/PangramTest.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arjun_000 on 3/8/2016.
+ */
+public class PangramTest {
+
+    @Test
+    public void testIsPangramNoExtraSpace() throws Exception {
+
+    }
+
+    @Test
+    public void testIsPangramExtraSpace() throws Exception {
+
+    }
+}
