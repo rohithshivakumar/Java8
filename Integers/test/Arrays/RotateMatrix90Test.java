c84986faafd8cbb72f937f7c8e35c8e62cccfd34
Rohith Shivakumar <rohshiva@cisco.com>
2016-03-29 21:29:47 -0700

    Rotate matrix by 90 degree

--- Integers/test/Arrays/RotateMatrix90Test.java
+++ Integers/test/Arrays/RotateMatrix90Test.java
@@ -22,7 +22,7 @@ public class RotateMatrix90Test {
     }
 
     @Test
-    public void testRotateMatrix() throws Exception {
+    public void testRotateMatrixRight() throws Exception {
         int[][] matrix = new int[][]{{1,2,3},{4,5,6}};
         int[][] expected =  new int[][]{{4,1},{5,2},{6,3}};
         RotateMatrix90 rotateMatrix90 = new RotateMatrix90();
@@ -32,6 +32,15 @@ public class RotateMatrix90Test {
         matrix = new int[][]{{1,2},{3,4}};
         expected =  new int[][]{{3,1},{4,2}};
         result = rotateMatrix90.rotateMatrixRightNotInplace(matrix);
+        assertArrayEquals(result,expected);
+    }
+
+    @Test
+    public void testRotateMatrixLeft() throws Exception {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        int[][] expected = new int[][]{{3,6,9,12},{2,5,8,11},{1,4,7,10}};
+        RotateMatrix90 rotateMatrix90 = new RotateMatrix90();
+        int[][] result = rotateMatrix90.rotateMatrixLeftNotInPlace(matrix);
         assertArrayEquals(result,expected);
     }
 }
