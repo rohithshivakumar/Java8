6ed394dde02420b2b2c47d8337779b59ef7fff3e
Arjun Raja <arjun.raja@hbo.com>
2017-12-10 19:34:28 -0600

    S5s project

---
+++ dependency-s5s/src/main/java/com/s5s/tests/dependency/InstallationType.java
@@ -0,0 +1,11 @@
+package com.s5s.tests.dependency;
+
+/// <summary>
+/// Installation type is an enum to represent the way we installed the component
+/// </summary>
+public enum InstallationType
+{
+    Explicit,
+    Implicit,
+    NotInstalled
+}
